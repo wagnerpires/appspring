460fb86e48df3e54570361e5322435c38576576c
Wagner Pires <wagnerpires@hotmail.com>
2023-02-09 23:02:11 -0300

    Ajustes no uptime

--- src/main/java/com/wrtecnologia/appspring/controllers/UptimeApp.java
+++ src/main/java/com/wrtecnologia/appspring/controllers/UptimeApp.java
@@ -13,12 +13,13 @@ public class UptimeApp {
     @GetMapping("/up")
     public String uptimeApp() {
         long millis = System.currentTimeMillis() - start;
+
         String uptime = String.format("%02d:%02d:%02d:%02d",
                 TimeUnit.MILLISECONDS.toDays(millis),
-                TimeUnit.MILLISECONDS.toHours(millis),
-                TimeUnit.MILLISECONDS.toMinutes(millis),
-                TimeUnit.MILLISECONDS.toSeconds(millis) -
-                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+                TimeUnit.MILLISECONDS.toHours(millis)   - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis)),
+                TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+
         return String.format("<h3>Uptime App: (up %s)</h3>", uptime);
     }
 }
